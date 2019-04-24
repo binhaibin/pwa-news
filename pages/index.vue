f7ab669674b6e0e1a801f1abcbaf1efc92bdc50d
vũ ngọc hải <binhaibin123@gmail.com>
2019-04-24 12:51:30 +0700

    make it better

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-multiline">
+       <table>
+      <thead>
+        <tr>
+          <td>name</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="music in posts" :key="music.id">
+          <td> {{ music.thumbnail }}</td>
+        </tr>
+      </tbody>
+    </table>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+   data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // lấy dữ liệu khi component được tạo thành công
+  created() {
+    axios.get(`https://localhost:44391/api/Songs`)
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+};
+</script>
